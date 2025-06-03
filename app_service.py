ab880eeba92777e5e176924412d966cdf0d6f074
pjsoto <pedrosoto423@gmail.com>
2025-06-03 13:16:16 +0200

    Including in the chain of procedures the ones related to SW fragments and Morphology

--- app_service.py
+++ app_service.py
@@ -1,4 +1,5 @@
 import io
+import sys
 import numpy as np
 from PIL import Image
 from scipy.stats import mode
@@ -28,44 +29,86 @@ def get_filenames():
 @app.route('/predict', methods=['POST'])
 def get_images():
     global FILE_NAMES
-    global MODELS
-    print("fue llamado")
-    print(FILE_NAMES)
-    print(MODELS)
+    global MODELS_
+    #print("fue llamado")
+    #print(FILE_NAMES)
+    #print(MODELS_)
     images = request.files.getlist("image")
     results = []
     for i, file in enumerate(images):
         img = np.array(Image.open(file.stream).convert("RGB"))[np.newaxis, :,:,:]
-        results_image = []
         # Preprocessing the image
-        counter = 0
-        for architecture, model_instance in MODELS.items():
-            predictions = model_instance.predict(img, architecture)
-            # Storing the predictions in a matrix per image in the way of [number of predictions, class]
-            if results_image == []:
-                results_image = np.zeros((len(ARCHITECTURES) * len(predictions), predictions[0].shape[1]))
-            for prediction in predictions:
-                results_image[counter, :] = prediction[0]
-                counter += 1
-        # Converting each prediction to a class in binary format
-        results_image_c = np.argmax(results_image, axis=1)
-        # Applying majority voting to the predictions
-        majority_voting = mode(results_image_c, axis=0)[0]
-        # Computing the predictive entropy of the predictions likelihood
-        predicitve_entropy = -1 * np.mean(np.mean(results_image, axis=0) * np.log(np.mean(results_image, axis=0) + 1e-10))
-        print(predicitve_entropy)
-        # Storing the results in a dictionary
-        results.append({
-            "filename": FILE_NAMES[i],
-            "predictions": {
-                CRITERIA[0]: {
-                    "class_number": class_dict[CRITERIA[0]]["class_number"],
-                    "predicted_index": int(majority_voting[0]),
-                    "uncertainty": float(predicitve_entropy),
-                    "predicted_class": class_dict[CRITERIA[0]]["class_names"][int(majority_voting[0])]
+        for CRITERIA_, MODELS  in MODELS_.items():
+            counter = 0
+            results_image = []
+            print("Processing image: ", FILE_NAMES[i], " for criteria: ", CRITERIA_)
+            for architecture, model_instance in MODELS.items():
+                predictions = model_instance.predict(img, architecture)
+                # Storing the predictions in a matrix per image in the way of [number of predictions, class]
+                if results_image == []:
+                    results_image = np.zeros((len(ARCHITECTURES) * len(predictions), predictions[0].shape[1]))
+                for prediction in predictions:
+                    results_image[counter, :] = prediction[0]
+                    counter += 1
+            #print(results_image)
+            # Converting each prediction to a class in binary format
+            if CRITERIA_ != CRITERIA[2]:
+                results_image_c = np.argmax(results_image, axis=1)
+                #Applying majority voting to the predictions
+                majority_voting = mode(results_image_c, axis=0)[0]
+                #Computing the predictive entropy of the predictions likelihood
+                predicitve_entropy = -1 * np.mean(np.mean(results_image, axis=0) * np.log(np.mean(results_image, axis=0) + 1e-10))
+                #print(predicitve_entropy)
+                #Storing the results in a dictionary
+                if len(results) == 0:
+                    results.append({
+                        "filename": FILE_NAMES[i],
+                        "predictions": {
+                            CRITERIA_: {
+                                "class_number": class_dict[CRITERIA_]["class_number"],
+                                "predicted_index": int(majority_voting[0]),
+                                "uncertainty": float(predicitve_entropy),
+                                "predicted_class": class_dict[CRITERIA_]["class_names"][int(majority_voting[0])]
+                            }
+                        }
+                    })
+                elif results[-1]["filename"] == FILE_NAMES[i]:
+                    results[-1]["predictions"][CRITERIA_] = {
+                        "class_number": class_dict[CRITERIA_]["class_number"],
+                        "predicted_index": int(majority_voting[0]),
+                        "uncertainty": float(predicitve_entropy),
+                        "predicted_class": class_dict[CRITERIA_]["class_names"][int(majority_voting[0])]
+                    }
+                else:
+                    results.append({
+                        "filename": FILE_NAMES[i],
+                        "predictions": {
+                            CRITERIA_: {
+                                "class_number": class_dict[CRITERIA_]["class_number"],
+                                "predicted_index": int(majority_voting[0]),
+                                "uncertainty": float(predicitve_entropy),
+                                "predicted_class": class_dict[CRITERIA_]["class_names"][int(majority_voting[0])]
+                            }
+                        }
+                    })
+            else:
+                #Storing the results in a dictionary
+                results_image_c = ((results_image > 0.5) * 1.0)
+                majority_voting_ = np.sum(results_image_c, axis=0)
+                majority_voting = [i for i, x in enumerate(majority_voting_) if x > 4]
+                for j in range(len(majority_voting)):
+                    if j == 0:
+                        indexs = str(majority_voting[j])
+                        class_name = class_dict[CRITERIA_]["class_names"][int(majority_voting[j])]
+                    else:
+                        indexs += ", " + str(majority_voting[j])
+                        class_name += ", " + class_dict[CRITERIA_]["class_names"][int(majority_voting[j])]
+                results[-1]["predictions"][CRITERIA_] = {
+                    "class_number": class_dict[CRITERIA_]["class_number"],
+                    "predicted_index": indexs,
+                    "uncertainty": 0.0,
+                    "predicted_class": class_name,
                 }
-            }
-        })
     print("Predictions finished successfully!!!")
     FILE_NAMES = []
     return jsonify({'results': results})
@@ -75,11 +118,30 @@ if __name__ == '__main__':
     # Initializing the previously trained segmentation models
     
     global MODELS
-    MODELS = {}
+    MODELS_ = {}
+    # Load the models for lithology and the different architectures
+    MODELS_LITHOLOGY = {}
     for architecture in ARCHITECTURES:
         print("Loading model for: ", CRITERIA[0], " and architecture: ", architecture)
-        models = Models(criteria=CRITERIA[0], architecture=architecture)
-        MODELS[architecture] = models
+        MODELS_LITHOLOGY[architecture] = Models(criteria=CRITERIA[0], architecture=architecture)
+    
+    MODELS_SW = {}
+    for architecture in ARCHITECTURES:
+        print("Loading model for: ", CRITERIA[1], " and architecture: ", architecture)
+        MODELS_SW[architecture] = Models(criteria=CRITERIA[1], architecture=architecture)
+
+    MODELS_MORPHOLOGY = {}
+    for architecture in ARCHITECTURES:
+        print("Loading model for: ", CRITERIA[2], " and architecture: ", architecture)
+        MODELS_MORPHOLOGY[architecture] = Models(criteria=CRITERIA[2], architecture=architecture)
+
+    # Storing the models in a dictionary
+    MODELS_ = {
+        CRITERIA[0]: MODELS_LITHOLOGY,
+        CRITERIA[1]: MODELS_SW,
+        CRITERIA[2]: MODELS_MORPHOLOGY
+    }
+        
     print("Models loaded successfully")
     print("Starting Flask server...")
     # Start the Flask server
